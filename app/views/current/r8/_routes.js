af8d2be192c8c3fa60d4ea5688be03ab6e7d9af8
Sam Carter <sam.carter@and.digital>
2024-04-19 15:22:55 +0100

    Updated to add in filters

--- app/views/current/r8/_routes.js
+++ app/views/current/r8/_routes.js
@@ -4,36 +4,28 @@ const router = express.Router()
 
 var data = require('../../../data/qualifications.json');
 
-router.get('/reset-filters', function(request, response) {
-  // Resets the selected data
-  setInitialData(request, response);
-  response.redirect("/current/r7/search-results");
-})
-
-router.post('/set-default-data', function(request, response) {
-  // Resets the selected data
-  setInitialData(request, response);
-  response.redirect("/current/r7/search-results");
-})
-
 // Route search results
 router.post('/post-search-results', function(request, response) {
-  var searchTerm = request.session.data['qualification-search']
-  var qualifications = data.qualifications.filter(x => x.name.toLowerCase().includes(searchTerm.toLowerCase()));
-  request.session.data['awarding-organisations'] = setAwardingOrganisations(qualifications, request);
+  var qualifications = data.qualifications;
+
+  if (request.session.data['qualification-search'] != undefined) {
+    var searchTerm = request.session.data['qualification-search'];
+    qualifications = data.qualifications.filter(x => x.name.toLowerCase().includes(searchTerm.toLowerCase()));
+  }
+ 
   qualifications = filterQualificationYear(qualifications, request);
   qualifications = filterLevels(qualifications, request);
   qualifications = filterAwardingOrganisations(qualifications, request);
   request.session.data['result-count'] = qualifications.length;
   request.session.data['search-results'] = qualifications;
-  response.redirect("/current/r7/search-results");
+  response.redirect("/current/r8/search-results");
 })
 
 function filterQualificationYear(qualifications, request) {
   // if the data doesn't include the qualification year then just return the list of qualifications as nothing to filter out.
-  if (request.session.data['qualification-year'] == undefined || request.session.data['qualification-year'].length == 0) return qualifications;
+  if (request.session.data['awarding-date'] == undefined || request.session.data['awarding-date'].length == 0) return qualifications;
 
-  return qualifications.filter(x => x.beforeOrAfter2014 == request.session.data['qualification-year']);
+  return qualifications.filter(x => x.beforeOrAfter2014 == request.session.data['awarding-date']);
 }
 
 function filterLevels(qualifications, request) {
@@ -61,41 +53,9 @@ function filterLevels(qualifications, request) {
 
 function filterAwardingOrganisations(qualifications, request) {
   // if the data doesn't include the awarding organisation filter, then return the lot
-  if (request.session.data['awarding-organisation'] == undefined || request.session.data['awarding-organisation'] == 'none') return qualifications;
+  if (request.session.data['awarding-organisation'] == undefined || request.session.data['awarding-organisation'] == 'none' || request.session.data['awarding-organisation'] == 'any') return qualifications;
 
   return qualifications.filter(x => x.awardingOrganisation == request.session.data['awarding-organisation']); 
-}
-
-function compareByText(a, b){
-  return a.text.localeCompare(b.text);
-}
-
-function setAwardingOrganisations(qualifications, request) {
-  var awardingOrganisations = qualifications.map(x => x.awardingOrganisation);
-  let uniqueAwardingOrganisations = [...new Set(awardingOrganisations)];
-  var formattedAwardingOrganisations = [{"value": "none", text: "--Select--"}];
-  uniqueAwardingOrganisations.forEach(element => {
-    if (request.session.data['awarding-organisation'] != undefined && element == request.session.data['awarding-organisation']) {
-      formattedAwardingOrganisations.push({"value": element, "text": element, "selected": true});
-    } else {
-      formattedAwardingOrganisations.push({"value": element, "text": element});
-    }
-  });
-  return formattedAwardingOrganisations.sort(compareByText);
-}
-
-function setInitialData(request, response){
-  var resetData = {};
-  // reset level checked to false
-  for (var i = 2; i <= 7; i++) {
-    var levelChecked = `level-${i}-checked`;
-    resetData[levelChecked] = false;
-  }
-  request.session.data = resetData;
-  var qualifications = data.qualifications;
-  request.session.data['result-count'] = qualifications.length;
-  request.session.data['search-results'] = qualifications;
-  request.session.data['awarding-organisations'] = setAwardingOrganisations(qualifications, request);
 }
 
 module.exports = router
