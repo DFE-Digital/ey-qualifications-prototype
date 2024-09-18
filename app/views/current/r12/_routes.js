9bbabb5774ad31ad31e41092f05897105bc19893
lsanchezhernandez <laura.sanchez@hippodigital.co.uk>
2024-09-18 14:22:45 +0100

    Added requirement question 3 and updated routes

--- app/views/current/r12/_routes.js
+++ app/views/current/r12/_routes.js
@@ -84,6 +84,15 @@ router.post('/eyq-500-requirement2-post', function(request, response) {
   if (request.session.data['includesAssessedPractice'] == undefined) {
     return response.redirect(`/current/r12/eyq-500-requirement2-error`)
   } else {
+    return response.redirect(`/current/r12/eyq-500-requirement3`)
+  }
+})
+
+router.post('/eyq-500-requirement3-post', function(request, response) {
+  var redirectValue = request.session.data['redirect'];
+  if (request.session.data['includesAssessedPractice'] == undefined) {
+    return response.redirect(`/current/r12/eyq-500-requirement3-error`)
+  } else {
     return response.redirect(`/current/r12/eyq-500-requirements-check`)
   }
 })
